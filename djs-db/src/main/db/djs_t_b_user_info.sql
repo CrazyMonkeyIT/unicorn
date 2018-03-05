df5d697d006f75dc4879f5723eb6fee19188ef85
biao.chenit@qq.com <bill572004>
2018-03-05 22:07:43 +0800

    sql 脚本

--- djs-db/src/main/db/djs_t_b_user_info.sql
+++ djs-db/src/main/db/djs_t_b_user_info.sql
@@ -16,3 +16,20 @@ CREATE TABLE `t_b_user_info` (
 LOCK TABLES `t_b_user_info` WRITE;
 INSERT INTO `t_b_user_info` VALUES (1,'chenbiao','bill','a02f6f0009b898916d0794508cd508652df76d3e1093e9198b2e2b84802a4b92',1,NULL,'2018-02-08 09:30:13','2018-02-08 10:20:56','2018-02-08 10:20:56');
 UNLOCK TABLES;
+
+--函数
+DROP FUNCTION IF EXISTS `getChildList`;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `getChildList`(rootId int) RETURNS varchar(2000) CHARSET utf8
+BEGIN
+DECLARE str varchar(2000);
+DECLARE cid varchar(100);
+SET str = '';
+SET cid = rootId;
+WHILE cid is not null DO
+    SET str = concat(str, ',', cid);
+    SELECT group_concat(user_id) INTO cid FROM t_b_login_user where FIND_IN_SET(parent_id, cid) > 0;
+END WHILE;
+RETURN substring(str,4);
+END
+;;
