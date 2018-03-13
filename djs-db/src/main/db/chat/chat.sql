8de1a3b7c21f06a503f6c7b8dc97c8e4af0df9a7
biao.chen <biao.chen@baozun.com>
2018-03-13 20:26:31 +0800

    pom调整

--- djs-db/src/main/db/chat/chat.sql
+++ djs-db/src/main/db/chat/chat.sql
@@ -1,11 +1,10 @@
-DROP TABLE IF EXISTS `t_b_content`;
-CREATE TABLE `t_b_content` (
+DROP TABLE IF EXISTS `t_b_room_content`;
+CREATE TABLE `t_b_room_content` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `room_id` bigint(20) DEFAULT NULL COMMENT '房间ID',
   `user_id` bigint(20) DEFAULT NULL COMMENT '用户ID',
   `content` text COMMENT '内容',
-  `attachment` blob COMMENT '附件',
-  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `create_time` datetime DEFAULT NULL COMMENT '记录创建时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -17,9 +16,14 @@ CREATE TABLE `t_b_room` (
   `name` varchar(255) DEFAULT NULL COMMENT '房间名称',
   `type` int(11) DEFAULT NULL COMMENT '房间內型',
   `count` int(11) DEFAULT NULL COMMENT '房间总人数',
-  `members` varchar(255) DEFAULT NULL COMMENT '房间人员ID',
+  `members` text DEFAULT NULL COMMENT '房间人员ID',
   `status` int(11) DEFAULT NULL COMMENT '房间状态',
-  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `room_price` decimal DEFAULT null comment '房间价格',
+  `prepare_live_begin_time` datetime DEFAULT NULL COMMENT '预计直播开始时间',
+  `prepare_live_end_time` datetime DEFAULT NULL COMMENT '预计直播结束时间',
+  `actual_live_begin_time` datetime default null comment '实际直播开始时间',
+  `actual_live_end_time` datetime default null comment '实际直播结束时间',
+  `create_time` datetime DEFAULT NULL COMMENT '记录创建时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
