99b6922fb31c3f7f2108fabf76feb415240e10e6
lee <l_joose@163.com>
2018-05-10 23:41:37 +0800

    小程序布局调整

--- djs-db/src/main/java/com/valueservice/djs/db/entity/chat/SubjectDO.java
+++ djs-db/src/main/java/com/valueservice/djs/db/entity/chat/SubjectDO.java
@@ -15,6 +15,26 @@ public class SubjectDO {
 
     private String subjectPoster;
 
+    private String startTimeStr;
+
+    private String endTimeStr;
+
+    public String getStartTimeStr() {
+        return startTimeStr;
+    }
+
+    public void setStartTimeStr(String startTimeStr) {
+        this.startTimeStr = startTimeStr;
+    }
+
+    public String getEndTimeStr() {
+        return endTimeStr;
+    }
+
+    public void setEndTimeStr(String endTimeStr) {
+        this.endTimeStr = endTimeStr;
+    }
+
     public Integer getSubjectId() {
         return subjectId;
     }
